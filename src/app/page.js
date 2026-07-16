b540f75cbd5019efa452c9a546514de2141f2a4b
mz-devtech <zeeshan.fullstack.developer@gmail.com>
2026-07-16 14:56:28 +0500

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -34,6 +34,7 @@ export default function Home() {
             center.
           </p>
         </div>
+      <h1 className="bg"></h1>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
